44e8e10d2bf34bcbfa4a15fb2f817a44ecccc9a6
Challenged <74725613+ChallengedFPS@users.noreply.github.com>
2023-10-03 22:07:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+y = 0
 def test(x):
-for i in range(len(x),-1,-1):
-	print(x[i-1],end="")
+y+=1
+if y == len(x):
+	pass
+else:
+	for i in range(len(x),-1,-1):
+		print(x[i-1],end="")
+	
 		
 		
 test("hello")
